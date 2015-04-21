4694fb1f566fe8bb2b501b0495b8392f7479a482
Konrad Maurer <kradjr@gmail.com>
2015-04-21 11:57:03 -0600

    mail views created

--- app/mailers/mail_mailer.rb
+++ app/mailers/mail_mailer.rb
@@ -1,2 +1,8 @@
 class MailMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+
+  def reminder_email(recipient)
+    mail(to: 'recipient@email.com', subject: 'Minutely Reminder')
+  end
 end
